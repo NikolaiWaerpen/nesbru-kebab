cbe4b039b67f28af64dc1dcf86a509ed05509409
waerpen <nikolai.waerpen@intility.no>
2021-01-11 19:19:53 +0100

    need to get rid of .next directory

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2a936d148e758c5e24c5";
+/******/ 	var hotCurrentHash = "6d64c1052749260665e6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
