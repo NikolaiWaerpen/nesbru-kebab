ed0b192363296bf5831db25014bde6b2c2e24823
waerpen <nikolai.waerpen@intility.no>
2021-01-13 02:25:48 +0100

    further menu changes

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -15,7 +15,7 @@ export default function Menu() {
             alt="pizza"
             className="hidden lg:block lg:w-screen/2 lg:h-screen/2 overflow-hidden object-cover itemcontent rounded-r-3xl"
           />
-          <div className="lg:flex lg:flex-col lg:justify-center lg:ml-12">
+          <div className="lg:flex lg:flex-col lg:justify-center lg:ml-20 lg:w-1/6">
             <h2 className="font-bold menutextcolorheader">Burgermeny</h2>
             {BURGERMENU.map((hamburger) => (
               <BurgerItem
@@ -32,9 +32,9 @@ export default function Menu() {
           <img
             src="/images/pizza.jpg"
             alt="pizza"
-            className="hidden lg:block lg:w-screen/2 lg:max-h-full overflow-hidden object-cover itemcontent rounded-l-3xl"
+            className="hidden lg:block lg:max-w-screen-md lg:max-h-full overflow-hidden object-cover itemcontent rounded-l-3xl"
           />
-          <div className="lg:flex lg:flex-col lg:justify-center lg:ml-12">
+          <div className="lg:flex lg:flex-col lg:justify-center lg:mr-20 lg:w-1/4">
             <h2 className="font-bold menutextcolorheader">Pizzameny</h2>
             {PIZZAMENU.map((pizza) => (
               <PizzaItem
@@ -52,7 +52,7 @@ export default function Menu() {
             alt="pizza"
             className="hidden lg:block lg:w-screen/2 lg:h-screen/2 overflow-hidden object-cover itemcontent rounded-r-3xl"
           />
-          <div className="lg:flex lg:flex-col lg:justify-center lg:ml-12">
+          <div className="lg:flex lg:flex-col lg:justify-center lg:ml-20 lg:w-1/6">
             <h2 className="font-bold menutextcolorheader">Grillmeny</h2>
             {GRILLMENU.map((grillitem) => (
               <GrillItem name={grillitem.name} price={grillitem.price} />
