5946d8a4ee80af0c17e07ffaee2057b42b78925c
waerpen <nikolai.waerpen@intility.no>
2021-01-13 23:34:49 +0100

    made signature even less visible

--- src/components/Signature/index.tsx
+++ src/components/Signature/index.tsx
@@ -1,7 +1,7 @@
 export default function Signature() {
   return (
     <div>
-      <h1 className="text-gray-500 flex justify-center">
+      <h1 className="text-gray-300 flex justify-center">
         Developed by Nikolai Wærpen
       </h1>
     </div>
