dfce3c0b819bf2e7b908f1cbebc6f4b9114d80d7
waerpen <nikolai.waerpen@intility.no>
2021-01-14 14:41:34 +0100

    banner background changes

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -3,18 +3,16 @@ import { SCROLL_DURATION } from "../../../consts";
 
 export default function Banner() {
   return (
-    <div>
-      <div className="relative z-0">
-        <img
-          src="/images/flour-big.jpg"
-          alt="logo picture/burger"
-          className="w-screen max-h-screen object-cover"
-        />
-        <div className="absolute inset-0 flex justify-center top-3/4">
-          <Link to="menu" smooth={true} duration={1000}>
-            <button className="button">Se meny</button>
-          </Link>
-        </div>
+    <div className="relative z-0 bg-offblack">
+      <img
+        src="/images/flour-big.jpg"
+        alt="logo picture/burger"
+        className="w-screen h-screen object-cover opacity-25"
+      />
+      <div className="absolute inset-0 flex justify-center top-3/4">
+        <Link to="menu" smooth={true} duration={1000}>
+          <button className="button">Se meny</button>
+        </Link>
       </div>
     </div>
   );
