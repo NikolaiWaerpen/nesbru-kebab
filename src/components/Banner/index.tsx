cd3396ce88c284369c022e4d40efe381883a8578
waerpen <nikolai.waerpen@intility.no>
2021-01-15 18:59:13 +0100

    switched to kebab in banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -14,12 +14,12 @@ export default function Banner() {
           Nesbru kebab
         </h1>
       </div> */}
-      <div className="absolute inset-0 flex justify-center z-20 lg:mx-0 top-32 lg:top-32">
+      <div className="absolute inset-0 flex justify-center z-20 lg:mx-0 top-64 lg:top-32">
         <img
-          src="/images/blackburger.png"
+          src="/images/transparentkebab.png"
           // src="/images/nesbrukebab/nesbrukebabwhite.png"
           alt="logo picture/burger"
-          className="w-full h-screen58 object-cover lg:h-screen lg:object-contain"
+          className="w-full h-screen29 object-contain lg:h-screen lg:object-contain opacity-90"
         />
       </div>
       <div className="absolute inset-0 flex justify-center top-3/4 z-40">
