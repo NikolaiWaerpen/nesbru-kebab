2ba371db891d8cc256b4ea55ad327a10407ffc32
waerpen <nikolai.waerpen@intility.no>
2021-01-15 03:41:23 +0100

    some consistency issues resolved

--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -3,17 +3,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Social() {
   return (
-    <div className="flex">
-      <a
-        href="https://www.facebook.com/NesbruKebab"
-        className="texthovereffect flex"
-      >
-        <FontAwesomeIcon
-          icon={faFacebookSquare}
-          className="text-4xl mr-2 block"
-        />
-        <h3 className="itemheader mt-0.5">Følg oss på Facebook!</h3>
-      </a>
+    <div>
+      {/* <h3 className="smallbadge">Sosiale media</h3> */}
+      <div>
+        <a
+          href="https://www.facebook.com/NesbruKebab"
+          className="texthovereffect flex justify-center"
+        >
+          <FontAwesomeIcon
+            icon={faFacebookSquare}
+            className="text-4xl mr-2 block"
+          />
+          <h3 className="itemheader">Følg oss på Facebook!</h3>
+        </a>
+      </div>
     </div>
   );
 }
